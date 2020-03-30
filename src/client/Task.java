2528f3884e2eb25c688a80c653c7c78e0c4f5220
liuchenailq <2422701543@qq.com>
2020-03-30 21:18:26 +0800

    first

---
+++ src/client/Task.java
@@ -0,0 +1,88 @@
+package client;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * @author LiuChen
+ * @date 2020/3/30
+ */
+public class Task implements Runnable {
+    ArrayList<String> arrayList;
+    boolean stopFlag = false;
+    public Task(ArrayList<String> arrayList, boolean stopFlag){
+        this.arrayList = arrayList;
+        this.stopFlag = stopFlag;
+    }
+
+    @Override
+    public void run() {
+        String ip = "39.108.67.176";
+        int port = 1234;
+
+        try {
+            Socket socket = new Socket(ip, port);
+
+            OutputStream ops = socket.getOutputStream();
+            OutputStreamWriter opsw = new OutputStreamWriter(ops);
+            BufferedWriter bw = new BufferedWriter(opsw);
+
+            if(stopFlag){
+                bw.write( "yes\r\n");
+                bw.flush();
+            }else {
+                bw.write( "no\r\n");
+                bw.flush();
+            }
+
+            for(String line : arrayList){
+                String tmp = calc(line);
+                if(tmp != null){
+                    bw.write(tmp + "\r\n");
+                    bw.flush();
+                }
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 容量计算
+     * @param line
+     * @return
+     */
+    public String calc(String line){
+        int index = line.indexOf(' ');
+        String head = line.substring(0, index + 1);
+        String data = line.substring(index + 1, line.length());
+        String[] arr = data.split(",");
+        StringBuilder sb = new StringBuilder();
+        sb.append(head);
+        boolean flag = false;
+        for(String t : arr){
+            // 除法可能有改进的地方
+            float d = Float.parseFloat(t);
+            if(d > 2400.0){
+                int a = (int) (d / 2400);
+                sb.append(a);
+                sb.append(",");
+                flag = true;
+            }else{
+                sb.append(0);
+                sb.append(",");
+            }
+        }
+        if(flag){
+            return sb.substring(0, sb.length() - 1);
+        }else {
+            return null;
+        }
+    }
+}
---
+++ src/client/Version2.java
@@ -0,0 +1,59 @@
+package client;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 单线程读取文件、多线程处理并发送
+ * @author LiuChen
+ * @date 2020/3/30
+ */
+public class Version2 {
+
+    /**
+     * 阻塞策略
+     */
+    private static class CustomRejectedExecutionHandler implements RejectedExecutionHandler {
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            try {
+                executor.getQueue().put(r);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+
+        File file = new File("/liuchen/input_data.txt");
+        BufferedReader fbr = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+
+        int corePoolSize = 2;
+        ArrayBlockingQueue<Runnable> arrayBlockingQueue = new ArrayBlockingQueue<>(corePoolSize);
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(corePoolSize, corePoolSize, 0L, TimeUnit.MILLISECONDS, arrayBlockingQueue, new CustomRejectedExecutionHandler());
+
+
+        String line = "";
+        int batchSize = 100000;
+        while (line != null){
+            ArrayList<String> arrayList = new ArrayList<>(batchSize);
+            int batch = 0;
+            while (batch < batchSize && ((line = fbr.readLine()) != null)){
+                arrayList.add(line);
+                batch ++;
+            }
+            // 调用线程处理并发送
+            boolean stopFlag = line == null ? true : false;
+            Task task = new Task(arrayList, stopFlag);
+            threadPoolExecutor.submit(task);
+        }
+
+        System.out.println("总耗时： " + (System.currentTimeMillis() - startTime) / 1000 + " 秒");
+    }
+}
